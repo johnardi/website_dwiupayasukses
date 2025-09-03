202b7b3f6b27f4e5ddf09487848c1d717c088ec9
John Ardi <64395540+johnardi@users.noreply.github.com>
2025-09-03 13:34:13 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
 import createMiddleware from "next-intl/middleware";
-import { locales, defaultLocale } from "./i18n/config";
+import { routing } from "./i18n/routing";
 
-export default createMiddleware({ locales, defaultLocale });
+export default createMiddleware(routing);
+
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ["/", "/(en|id)/:path*", "/((?!api|trpc|_next|_vercel|.*\\..*).*)"],
+  // Match all pathnames except:
+  // - those starting with `/api`, `/trpc`, `/_next`, or `/_vercel`
+  // - those containing a dot (e.g. `favicon.ico`, `.png`, etc.)
+  matcher: ["/((?!api|trpc|_next|_vercel|.*\\..*).*)"]
 };
