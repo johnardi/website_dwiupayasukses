80002d879a8ce41659ae1a3862f6faed7b712cf4
John Ardi <64395540+johnardi@users.noreply.github.com>
2025-09-03 13:10:59 +0700

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig: NextConfig = {
+  distDir: "build",
+  images: {
+    unoptimized: true,
+  },
+};
+
+const withNextIntl = createNextIntlPlugin();
+
+export default withNextIntl(nextConfig);
